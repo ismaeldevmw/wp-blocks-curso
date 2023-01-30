03ac053bc978dead3ab7476c0ffeaf9bd127bfdc
Ismael López <ismaellopez.dev@gmail.comm>
2023-01-29 22:21:51 -0600

    Added API to fetch latest posts

--- news/src/edit.js
+++ news/src/edit.js
@@ -1,9 +1,38 @@
 import { useBlockProps } from '@wordpress/block-editor';
+import apiFetch from '@wordpress/api-fetch';
+import { useState, useEffect } from '@wordpress/element';
 
 const Edit = (props) => {
   const blockProps = useBlockProps();
+  const [posts, setPosts] = useState([]);
 
-  return <p {...blockProps}>Hola</p>;
+  const fetchPosts = async () => {
+    let path = '/wp/v2/posts/';
+    const newPosts = await apiFetch({ path });
+
+    setPosts(newPosts);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      {posts.length > 0 && (
+        <div {...blockProps}>
+          <h3>Quizas te interese leer esto:</h3>
+          <ul className="posts">
+            {posts.map((post) => (
+              <li key={post.id}>
+                <a href={post.link}>{post.title.rendered}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Edit;
